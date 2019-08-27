3f5cb48e38bfcd9b14dceb9563769f1d6a1fba97
ayang998 <52721276+ayang998@users.noreply.github.com>
2019-08-27 12:05:14 -0400

    Update Session.java

--- network/Session.java
+++ network/Session.java
@@ -1,36 +1,24 @@
 
-
 package network;
 
-/**
- * The {@code Session} class represents a session between this client and a
- * remote checkers client. It contains the important connection information
- * that is used to pass messages and accept messages.
- */
 public class Session {
 
-	/** The listener handling the connection on this client. */
 	private ConnectionListener listener;
-	
-	/** The session ID used for correspondence between the two clients. */
+
 	private String sid;
-	
-	/** The destination host name or IP. */
+
 	private String destinationHost;
-	
-	/** The destination port. */
+
 	private int destinationPort;
 
-	public Session(ConnectionListener listener, String sid,
-			String destinationHost, int destinationPort) {
+	public Session(ConnectionListener listener, String sid, String destinationHost, int destinationPort) {
 		this.listener = listener;
 		this.sid = sid;
 		this.destinationHost = destinationHost;
 		this.destinationPort = destinationPort;
 	}
-	
-	public Session(String sid, int sourcePort,
-			String destinationHost, int destinationPort) {
+
+	public Session(String sid, int sourcePort, String destinationHost, int destinationPort) {
 		this.listener = new ConnectionListener(sourcePort);
 		this.sid = sid;
 		this.destinationHost = destinationHost;
@@ -68,11 +56,11 @@ public class Session {
 	public void setDestinationPort(int destinationPort) {
 		this.destinationPort = destinationPort;
 	}
-	
+
 	public int getSourcePort() {
-		return (listener == null? -1 : listener.getPort());
+		return (listener == null ? -1 : listener.getPort());
 	}
-	
+
 	public void setSourcePort(int sourcePort) {
 		if (listener != null) {
 			this.listener.setPort(sourcePort);
