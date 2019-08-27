0e6c32595a77793059681a691f2c3499c3f9ca27
Anna Nguyen <an542na@gmail.com>
2019-08-27 08:29:26 -0400

    change location of resources

--- ui/SplashScreen.java
+++ ui/SplashScreen.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 public class SplashScreen extends JFrame{
 
 	public SplashScreen() {
-		ImageIcon splashScreen = new ImageIcon("../resources/splash-screen.png");
+		ImageIcon splashScreen = new ImageIcon("resources/splash-screen.png");
 		JLabel label = new JLabel(splashScreen);
 		add(label);
 		pack();
